1fef4984052bf871880d7a04f8c27cc9ca64d9b0
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2024-03-08 20:34:50 -0300

    Add student insert endpoint

--- src/main/java/com/tiagorodrigues/schoolapi/resources/StudentResource.java
+++ src/main/java/com/tiagorodrigues/schoolapi/resources/StudentResource.java
@@ -4,11 +4,10 @@ import com.tiagorodrigues.schoolapi.entities.Student;
 import com.tiagorodrigues.schoolapi.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +27,12 @@ public class StudentResource {
     public ResponseEntity<Student> findById(@PathVariable Long id) {
         Student obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> insert(@RequestBody Student obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- src/main/java/com/tiagorodrigues/schoolapi/services/StudentService.java
+++ src/main/java/com/tiagorodrigues/schoolapi/services/StudentService.java
@@ -22,4 +22,8 @@ public class StudentService {
         Optional<Student> obj = repository.findById(id);
         return obj.get();
     }
+
+    public Student insert(Student obj) {
+        return repository.save(obj);
+    }
 }
