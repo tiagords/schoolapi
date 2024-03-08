2c1578baa2e03947ba2cf90deb27a1a7888ecd9e
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2024-03-08 20:41:54 -0300

    Add student delete endpoint

--- src/main/java/com/tiagorodrigues/schoolapi/services/StudentService.java
+++ src/main/java/com/tiagorodrigues/schoolapi/services/StudentService.java
@@ -26,4 +26,8 @@ public class StudentService {
     public Student insert(Student obj) {
         return repository.save(obj);
     }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
