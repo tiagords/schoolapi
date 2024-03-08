78338d263b9383b28511d8e1d832fa4f8d1e0a55
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2024-03-07 21:03:28 -0300

    Add endpoint for list students

--- src/main/java/com/tiagorodrigues/schoolapi/resources/StudentResource.java
+++ src/main/java/com/tiagorodrigues/schoolapi/resources/StudentResource.java
@@ -1,18 +1,32 @@
 package com.tiagorodrigues.schoolapi.resources;
 
 import com.tiagorodrigues.schoolapi.entities.Student;
+import com.tiagorodrigues.schoolapi.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/students")
 public class StudentResource {
 
+    @Autowired
+    private StudentService service;
+
     @GetMapping
-    public ResponseEntity<Student> findAll() {
-        Student s = new Student(1L, "Tiago Rodrigues", "tiago@email.com", "19999999999");
-        return ResponseEntity.ok().body(s);
+    public ResponseEntity<List<Student>> findAll() {
+        List<Student> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Student> findById(@PathVariable Long id) {
+        Student obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/tiagorodrigues/schoolapi/services/StudentService.java
@@ -0,0 +1,25 @@
+package com.tiagorodrigues.schoolapi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.tiagorodrigues.schoolapi.entities.Student;
+import com.tiagorodrigues.schoolapi.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
+    public List<Student> findAll() {
+        return repository.findAll();
+    }
+
+    public Student findById(Long id) {
+        Optional<Student> obj = repository.findById(id);
+        return obj.get();
+    }
+}
